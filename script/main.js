640f4f899650ca941f5a02997271bd7a1f2358a6
モン <69784343+myat-mon@users.noreply.github.com>
2021-06-05 12:11:22 +0900

    add script

---
+++ script/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(function() {
+  console.log( "ready!" );
+  AOS.init();
+});
+
+function scrollToTargetElement(elementId) {
+  $('html, body').animate({
+    scrollTop: $(`#${elementId}`).offset().top
+  }, 500);
+}
