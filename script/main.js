f26a1f69da6b4485ffc2e8fb0a691a6046a74e71
Myat mon <69784343+myat-mon@users.noreply.github.com>
2021-09-27 14:16:20 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -5,14 +5,6 @@ $(function() {
   AOS.init();
 });
 
-function myFunction() {
-  var x = document.getElementById("mobile-demo");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
 
 function scrollToTargetElement(elementId) {
   $('html, body').animate({
