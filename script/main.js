4ed31518a45a813139494fcf7bf6e321acceacaf
Myat mon <69784343+myat-mon@users.noreply.github.com>
2021-09-27 14:22:35 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -5,6 +5,14 @@ $(function() {
   AOS.init();
 });
 
+function myFunction() {
+  var x = document.getElementById("sidenav-trigger");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
 function scrollToTargetElement(elementId) {
   $('html, body').animate({
